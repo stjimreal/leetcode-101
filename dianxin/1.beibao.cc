49a247ee10e9352ac73e92624d7c1643366dcb2e
jimlau <stjimreal@gmail.com>
2021-10-11 17:36:09 +0800

    背包状态压缩

--- dianxin/1.beibao.cc
+++ dianxin/1.beibao.cc
@@ -1,7 +1,7 @@
 /*
  * @Date: 2021-10-09 20:29:15
  * @LastEditors: LIULIJING
- * @LastEditTime: 2021-10-10 18:46:46
+ * @LastEditTime: 2021-10-11 17:33:28
  */
 #include <iostream>
 #include <vector>
@@ -11,7 +11,7 @@ using namespace std;
 const int N = 10005;
 vector<int> bottle{1,5,10,20,50,100};
 int main() {
-    unsigned long dp[6][N];
+    unsigned long dp[N];
     int n;
     cin>>n;
     if (n < 1) {
@@ -19,29 +19,17 @@ int main() {
         return 0;
     }
     memset(dp, 0, N * sizeof(unsigned long));
-    for (int i = 0; i < 6; ++i) {
-        dp[i][0] = 1;
-    }
-    for (int i = 1; i <= n; ++i) {
-        dp[0][i] = 1;
+    for (int i = 0; i <= n; ++i) {
+        dp[i] = 1;
     }
     for (int i = 1; i < 6; ++i) {
         for (int j = 1; j <= n; ++j) {
-            dp[i][j] = (j >= bottle[i]? dp[i][j - bottle[i]]: 0) + dp[i - 1][j];
-        }
-    }
-    for (int i = 0; i < 6; ++i) {
-        for (int j = 0; j <= n; ++j) {
-            cout<<dp[i][j]<<" ";
+            dp[j] = (j >= bottle[i]? dp[j - bottle[i]]: 0) + dp[j];
+            cout<<dp[j]<<" ";
         }
         cout<<endl;
     }
 
-    // for (int i = 1; i <= n; ++i) {
-    //     for (int j = 0; j < 6; ++j) {
-    //         dp[i] += i >= bottle[j]? dp[i-bottle[j]]: 0;
-    //     }
-    // }
-    cout<<dp[5][n]<<endl;
+    cout<<dp[n]<<endl;
     return 0;
 }
